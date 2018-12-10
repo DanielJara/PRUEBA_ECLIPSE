c82260af8e635cce01fcd7a3d023ab27f3c346cc
Daniel Jara <danieljara773@gmail.com>
2018-12-10 09:05:20 +0100

    LEYENDO DATOS

--- src/Humano.java
+++ src/Humano.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class Humano {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hola");		
 	}
 
 }
